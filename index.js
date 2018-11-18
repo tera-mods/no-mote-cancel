5bf26848889f67d89feedd7a631060bc77cd9635
pinkipi <yarnovax@gmail.com>
2018-11-17 21:28:40 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const MOTES = [18, 22]
+
+module.exports = function NoMoteCancel(mod) {
+	let job = -1
+
+	mod.hook('S_LOGIN', 10, event => { job = event.templateId % 100 - 1 })
+	mod.hook('C_CANCEL_SKILL', 3, event => job === 7 && MOTES.includes(Math.floor(event.skill.id / 10000)) ? false : undefined)
+}
