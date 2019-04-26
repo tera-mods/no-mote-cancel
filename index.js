c53bda03562f3347b5178169641bdfd2d5d4e4bc
pinkipi <yarnovax@gmail.com>
2019-04-26 06:58:10 -0400

    Patch 81 update

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const MOTES = [18, 22]
 module.exports = function NoMoteCancel(mod) {
 	let job = -1
 
-	mod.hook('S_LOGIN', 12, event => { job = event.templateId % 100 - 1 })
+	mod.hook('S_LOGIN', mod.patchVersion < 81 ? 12 : 13, event => { job = event.templateId % 100 - 1 })
 	mod.hook('C_CANCEL_SKILL', 3, event => job === 7 && MOTES.includes(Math.floor(event.skill.id / 10000)) ? false : undefined)
 }
